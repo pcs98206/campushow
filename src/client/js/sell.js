89c7ea68d2b6340579aa72a6a80068e1a3603221
Sangkyu <i9704@naver.com>
2022-05-18 21:30:44 +0900

    semester change
    현재 월에 따라 학기 추가

--- src/client/js/sell.js
+++ src/client/js/sell.js
@@ -66,8 +66,14 @@ const addSemesterOpt = () => {
     const optList = [];
     for(i=0; i<20; i++){
         const year = `${new Date().getFullYear()}`;
-        optList.push(`${year-i}`+"년 2학기");
-        optList.push(`${year-i}`+"년 1학기");
+        const month = new Date().getMonth();
+        if(month >= 1 && month < 9){
+            optList.push(`${year-i}`+"년 1학기");
+            optList.push(`${year-i-1}`+"년 2학기");
+        }else{
+            optList.push(`${year-i}`+"년 2학기");
+            optList.push(`${year-i}`+"년 1학기");
+        }
         const opt = document.createElement("option");
         opt.value=optList[i];
         opt.innerText=optList[i];
