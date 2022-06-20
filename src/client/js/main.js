d1577496bde2c74a3dbad9ae5c8f6553fc37938e
Sangkyu <i9704@naver.com>
2022-06-20 20:12:24 +0900

    SCSS forms
    1. login
    2. join
    3. profile
    4. sell

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,6 +1,16 @@
 import "../scss/styles.scss";
 
+const filename = document.querySelector('.input_filename');
+const file = document.querySelector('.input_file');
 
 document.addEventListener("contextmenu", e => {
     e.target.matches("img") && e.preventDefault()
-})
+});
+
+const filenameChange = () => {
+    filename.value = file.files[0].name;
+};
+
+if(file){
+    file.addEventListener('change', filenameChange);
+}
