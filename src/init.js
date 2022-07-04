4ffad13cdb4b928b088c0b0d75680e2933364bec
Sangkyu <i9704@naver.com>
2022-07-04 20:47:44 +0900

    build
    1. build backend
    2. build frontend

--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+import regeneratorRuntime from "regenerator-runtime";
 import 'dotenv/config';
 import app from "./server";
 import "./db";
--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,4 @@ module.exports = {
             }
         ],
     },
-    mode: 'development',
-    watch: true
 };
